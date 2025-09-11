eafbbf752742d3cedc11b0d1b5e7eda62cbd26c6
Joel Yovera <ingyovera6114@gmail.com>
2025-09-11 01:14:12 -0500

    feat: agregar validaciones para los registros de las entidades Hospital y Espcializacion

--- src/main/java/com/medx/beta/model/Especializacion.java
+++ src/main/java/com/medx/beta/model/Especializacion.java
@@ -1,6 +1,8 @@
 package com.medx.beta.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,9 +19,12 @@ public class Especializacion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer especializacionId;
 
-    @Column(nullable = false, length = 100)
+    @NotBlank(message = "El nombre de la especialización es obligatorio")
+    @Size(max = 70, message = "El nombre no debe exceder los 70 caracteres")
+    @Column(nullable = false, length = 70)
     private String nombre;
 
+    @Size(max = 255, message = "La descripción no puede exceder los 255 caracteres")
     @Column(length = 255)
     private String descripcion;
 
--- src/main/java/com/medx/beta/model/Hospital.java
+++ src/main/java/com/medx/beta/model/Hospital.java
@@ -1,13 +1,17 @@
 package com.medx.beta.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
 @Entity
-@Table(name = "Hospital")
+@Table(name = "Hospital", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "nombre", name = "uk_hospital_nombre")
+})
 
 @Getter
 @Setter
@@ -17,9 +21,12 @@ public class Hospital {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer hospitalId;
 
+    @NotBlank(message = "El nombre del hospital es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
     @Column(nullable = false, length = 100)
     private String nombre;
 
+    @Size(max = 500, message = "La descripción no puede exceder los 500 caracteres")
     @Column(columnDefinition = "TEXT")
     private String descripcion;
 
